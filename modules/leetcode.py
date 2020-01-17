1fe9225ff6def61da5675ffc9081b3f4f1f696ca
James Davidson <jedavidson2000@gmail.com>
2020-01-17 21:46:13 +1100

    Minor adjustment to imports for LeetCode module

--- modules/leetcode.py
+++ modules/leetcode.py
@@ -1,7 +1,7 @@
 # leetcode.py: Get a random LeetCode problem
 # by James Davidson
 
-import requests
+from requests import get
 
 random_problem_url = 'https://leetcode.com/problems/random-one-question/all'
 
@@ -10,5 +10,5 @@ random_problem_url = 'https://leetcode.com/problems/random-one-question/all'
 Return the URL of a random LeetCode problem.
 """
 def get_random_problem():
-    problem_url = requests.get(random_problem_url).url
+    problem_url = get(random_problem_url).url
     return problem_url
