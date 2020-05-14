e7536f88f9ea0cbfa18cf2c23edd196603db1d40
James Davidson <jedavidson2000@gmail.com>
2020-05-14 20:21:28 +1000

    Add clearly defined sections in vigenere module

--- modules/vigenere.py
+++ modules/vigenere.py
@@ -1,21 +1,32 @@
 # vigenere.py: Encipher a message with a Vigenere cipher
 # by James Davidson
 
-
 import re
 
 
-"""
-Return the shifted character according to the cipher key.
-The case of the original character is preserved.
-"""
-def encipher_char(ch: str, cipher_key: str) -> str:
-	shifted_ch = ord(ch.lower()) + ord(cipher_key.lower()) - ord('a')
-	if shifted_ch > ord('z'):
-		shifted_ch -= 26
-	shifted_ch = chr(shifted_ch)
+####################
+# Helper functions #
+####################
 
-	return shifted_ch.upper() if ch.isupper() else shifted_ch
+
+def _encipher_char(ch: str, cipher_key: str) -> str:
+    ''' Returns the shifted character according to the cipher key.
+        The case of the original character is preserved.
+    '''
+
+    shifted_ch = ord(ch.lower()) + ord(cipher_key.lower()) - ord('a')
+
+    if shifted_ch > ord('z'):
+        shifted_ch -= 26
+
+    shifted_ch = chr(shifted_ch)
+
+    return shifted_ch.upper() if ch.isupper() else shifted_ch
+
+
+######################
+# Main functionality #
+######################
 
 
 """
@@ -30,23 +41,25 @@ the keyword is filled to match the length of the plaintext:
 - 'c' feeds the ciphertext into the remaining space
 """
 def encipher(method: str, keyword: str, plaintext: str) -> str:
-	keyword = re.sub(r'[^a-zA-Z]', '', keyword)
-	no_punct = re.sub(r'[^a-zA-Z]', '', plaintext)
-	alpha_count = 0
-	ciphertext = ""
-	for ch in plaintext:
-		if ch.isalpha():
-			cipher_key = ""
-			if method == 'k' or alpha_count < len(keyword):
-				cipher_key = keyword[alpha_count % len(keyword)]
-			elif method == 'p':
-				cipher_key = no_punct[alpha_count - len(keyword)]
-			# TODO: fix ciphertext feedback
-			# elif method == 'c':
-			# 	...
-			ciphertext += encipher_char(ch, cipher_key)
-			alpha_count += 1
-		else:
-			ciphertext += ch
+    keyword = re.sub(r'[^a-zA-Z]', '', keyword)
+    no_punct = re.sub(r'[^a-zA-Z]', '', plaintext)
 
-	return ciphertext
+    alpha_count = 0
+    ciphertext = ""
+
+    for ch in plaintext:
+        if ch.isalpha():
+            cipher_key = ""
+            if method == 'k' or alpha_count < len(keyword):
+                cipher_key = keyword[alpha_count % len(keyword)]
+            elif method == 'p':
+                cipher_key = no_punct[alpha_count - len(keyword)]
+            # TODO: fix ciphertext feedback
+            # elif method == 'c':
+            # 	...
+            ciphertext += _encipher_char(ch, cipher_key)
+            alpha_count += 1
+        else:
+            ciphertext += ch
+
+    return ciphertext
